38b7f028a648461de97c6736341cecaa3740e7f0
Karla Šmuk <karla.smuk@gmail.com>
2024-10-08 17:51:48 +0200

    added new scripts to run code and run new migration

--- src/migrations/1728402503141-addedTicket.ts
+++ src/migrations/1728402503141-addedTicket.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddedTicket1727899637598 implements MigrationInterface {
-    name = 'AddedTicket1727899637598'
+export class AddedTicket1728402503141 implements MigrationInterface {
+    name = 'AddedTicket1728402503141'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "ticket" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "vatin" character varying NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d9a0835407701eb86f874474b7c" PRIMARY KEY ("id"))`);
