7d0ff22a14904f071c919ea1e48f74aadc3edcb1
Karla Šmuk <karla.smuk@gmail.com>
2024-10-16 20:03:26 +0200

    updated json for errors

--- backend/src/middleware/errors.ts
+++ backend/src/middleware/errors.ts
@@ -3,7 +3,7 @@ import { UnauthorizedError } from "express-oauth2-jwt-bearer";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof UnauthorizedError) {
-        res.status(401).json({ error: 'Unauthorized access' });
+        res.status(401).json({ message: 'Unauthorized access' });
     }
-    res.status(500).send({ errors: [{ message: "Something went wrong" }] });
+    res.status(500).send({ message: err.message });
 };
