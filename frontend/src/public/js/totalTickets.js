f27acc880ae66349fea9b1043fe709653f7f82a7
Karla Šmuk <karla.smuk@gmail.com>
2024-10-16 11:00:53 +0200

    wrap buttons login and logout with a tag

--- frontend/src/public/js/totalTickets.js
+++ frontend/src/public/js/totalTickets.js
@@ -1,7 +1,5 @@
 const BASE_URL = window.location.origin;
 const CHECK_AUTH_URL = "/isAuthenticated";
-const LOGIN_URL = "/login";
-const LOGOUT_URL = "/logout";
 
 async function getTotalNumOfTickets() {
   await fetch("http://localhost:3000/api/ticket/totalNumber")
@@ -10,14 +8,6 @@ async function getTotalNumOfTickets() {
       document.getElementById("total-tickets").innerText = data.total;
     });
 }
-
-const login = async () => {
-  window.location.href = BASE_URL + LOGIN_URL;
-};
-
-const logout = async () => {
-  window.location.href = BASE_URL + LOGOUT_URL;
-};
 
 const checkAuth = async () => {
   const response = await fetch(CHECK_AUTH_URL);
