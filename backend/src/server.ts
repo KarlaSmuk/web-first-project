471f2a43019f126d966dbf963636761eb8930e9c
Karla Šmuk <karla.smuk@gmail.com>
2024-10-21 20:08:14 +0200

    prepare backend for deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,12 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/ticket", ticketRoute)
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const PORT = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 3001;
 AppDataSource.initialize()
   .then(async () => {
-    app.listen(PORT, () => {
-      console.log("Server is running on http://localhost:" + PORT);
-    });
+    const hostname = '0.0.0.0';
+    if (externalUrl) {
+      app.listen(PORT, hostname, () => {
+        console.log(`Server locally running at http://${hostname}:${PORT}/ and from
+          outside on ${externalUrl}`);
+      });
+    } else {
+      app.listen(PORT, () => {
+        console.log("Server is running on http://localhost:" + PORT);
+      });
+    }
+
     console.log("Data Source has been initialized!");
   })
   .catch((error) => console.log(error));
