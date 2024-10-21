3a05b3d5e31baa9c97f0410702461798737c3489
Karla Šmuk <karla.smuk@gmail.com>
2024-10-21 20:55:06 +0200

    fix audience

--- backend/src/middleware/jwtCheck.ts
+++ backend/src/middleware/jwtCheck.ts
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer"
 
 export const jwtCheck = auth({
-    audience: process.env.RENDER_EXTERNAL_URL + '/',
+    audience: process.env.RENDER_EXTERNAL_URL,
     issuerBaseURL: process.env.ISSUER_URL,
     tokenSigningAlg: 'RS256'
 });
